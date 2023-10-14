8496669071cddc630a816db5effdc0fc323ac2fc
alona <alena19apr@gmail.com>
2023-10-15 01:38:06 +0200

    решение задачи

---
+++ src/lesson2/Homework1.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+import java.util.Arrays;
+
+public class Homework1 {
+    public static void main(String[] args) {
+      int a=7;
+      int b=9;
+        System.out.println(a=a+b%a);
+        System.out.println(b=--b-a%b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+
+}
